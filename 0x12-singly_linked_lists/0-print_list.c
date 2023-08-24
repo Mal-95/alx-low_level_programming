4573ef5d9a27837a44f7f0b2b20963c855ebaecd
Mal-95 <malebo445.ml@gmail.com>
2023-08-24 13:47:41 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,22 +4,32 @@
 /**
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
- *
- * Return: the number of nodes printed
+ * strct print_list - represents a linked list node with a string
+ * Return: the number of nodes in the linked list
  */
+typedef struct list_t
+{
+	char *str;
+	struct list_t *next;
+} list_t;
+
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		if (!h->str)
+		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
-			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
-		s++;
-	}
+		{
+			printf("[%lu] %s\n", count, h->str);
+		}
 
-	return (s);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
